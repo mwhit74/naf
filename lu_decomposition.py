631ab6a20ff4ff575e21e5730bac3c87d6ec957f
mwhit74 <mwhit74@gmail.com>
2018-01-22 20:35:56 -0600

    Working on LU Decomposition.
    Added comments to clarify parts of the code. Still clarifying the back
    substituation function.
    
    Added order vector to swap rows. Had to add this everywhere a row is referenced.
    
    May create two different functions. One to calculate the L and U matricies and
    one to calculate the back substitution. This will work well when the back
    substitution step needs to be done but not the whole LU decomposition. Will also
    need a function to multiply matricies; not sure if it will write this one or let
    numpy handle it.
    
    Update the indexing so hopefully the first column and row will be 0 instead of 1
    like the algorithem I am following has done.

--- lu_decomposition.py
+++ lu_decomposition.py
@@ -1,33 +1,46 @@
 import numpy as np
 
+def lu_decomp(A, b):
 n,m = A.shape
+ov = np.arange(n) #order_vector
+
+if n != m:
+    raise ValueError("Matrix is not sqaure.")
 
 #row reduction
-for j in xrange(n-1):
-    pvt = abs(A[j,j])
-    pivot[j] = j
-    ipvt_temp = j
+for j in xrange(n):
+    pvt = abs(A[ov[j],j]) #gets current pvt on diagonal
+    org_pvt_row = j #keeps track of row location of pvt
 
+    #cycle thru entries in first column
+    #find largest value
     for i in xrange(j+1,n):
         if abs(A[i,j]) > pvt:
             pvt = abs(A[i,j])
-            ipvt_temp = i
+            new_pvt_row = i
 
-    if pivot[j] != ipvt_temp:
-        switch_rows(j, ipvt_temp)
+    #switch largest value to be pivot
+    #this reduces rounding error
+    if org_pvt_row != new_pvt_row:
+        ov[org_pvt_row] = org_pvt_row
+        ov[new_pvt_row] = new_pvt_row
 
+    #calculates multipliers for row reduction
     for i in xrange(j+1,n):
-        A[i,j] = A[i,j]/A[j,j]
+        A[ov[i],j] = A[ov[i],j]/A[ov[j],j]
 
-    for i in xrange(j+1,n):
-        for k in xrange(j+1,n):
-            a[i,k] = a[i,k] - a[i,j]*a[j,k]
-        b[i] = b[i] - a[i,j]*b[j]
+    #creates zeros below the main diagonal
+    for i in xrange(j+1,n): #steps across columns
+        for k in xrange(j+1,n): #steps down rows in each column
+            A[ov[i],k] = A[ov[i],k] - A[ov[i],j]*A[ov[j],k]
+        b[ov[i]] = b[ov[i]] - a[ov[i],j]*b[ov[j]] #performs rhs calc
 
+def back_sub():
 #back substitution
-x[n] = b[n]/a[n,n]
+x[ov[n]] = b[ov[n]]/a[ov[n],n] # last row solution
 for j in xrange(n-1,1,-1):
-    x[j] = b[j]
+    x[ov[j]] = b[ov[j]]
     for k in xrange(n,j+1,-1):
-        x[j] = x[j] - x[k]*a[j,k]
-    x[j] = x[j]/a[j,j]
+        x[ov[j]] = x[ov[j]] - x[ov[k]]*a[ov[j],k]
+    x[ov[j]] = x[ov[j]]/a[ov[j],j]
+
