bca3de4bade60e54cf51e5525de60e5acbdc61a8
MLW <mwhit74@gmail.com>
2018-01-26 17:13:26 -0600

    LU decomposition works; forward elimination; back substitution; docs
    Implemented an LU decomposition that appears to be working. Used forward
    elimination and back substitution functions to finish the solution to the
    equation Ax=b.
    
    Also added documentation.
    
    Needs actual unit testing. To come soon.

--- linalg/lu.py
+++ linalg/lu.py
@@ -1,7 +1,32 @@
 import numpy as np
 import pdb
 
-def lu_decomp(A, b):
+
+def lu_decomp(A):
+    """Finds the LU decomposition of the matrix A.
+
+    This function factors the A matrix into two matricies L and U. L is a lower
+    triangular matrix of the multiplication coefficients with 1's on the
+    diagonal. U is an upper triangular matrix of the remaining coefficients of
+    the original A matrix after pivoting and row reduction.
+
+    This function does the decomposition in place. That is to say it stores the
+    resulting values in the A matrix. 
+
+    This function also keeps track of any row exchanges due to partial pivoting
+    of the rows. 
+
+    Args:
+        A (two dimensional numpy array): matrix to be factored into LU
+
+    Retuns:
+        A (two dimensional numpy array): combined L and U matricies; L is the
+            lower triangular with 1's on the diagonal; U is the upper triangular
+            including the values on the diagonal
+        ov (one dimensional numpy array): the permutated order of the rows after
+            partial pivoting and factorization
+
+    """
     n,m = A.shape
     #n minus one, shape returns the number of elements; not good for
     #indexing
@@ -38,59 +63,100 @@ def lu_decomp(A, b):
         for i in xrange(j+1,n): #row number
             for k in xrange(j+1,n): #column number
                 A[ov[i],k] = A[ov[i],k] - A[ov[i],j]*A[ov[j],k]
-            b[ov[i]] = b[ov[i]] - A[ov[i],j]*b[ov[j]] #performs rhs calc
     
-    return A, b, ov #where A is now L in the lower and U in the upper
+    return A, ov #where A is now L in the lower and U in the upper
 
-def back_sub(A, b, ov):
-    n,m = A.shape
+
+def for_elim(LU, b, ov):
+    """Forward elmination applied to the b vector.
+
+    Applies the operations recorded in the decomposition, the L lower triangular
+    matrix and the row exchanges, to the b vector. 
+
+    This step can be taken in the LU decomposition function but there are
+    significant efficienies to be gained by having a separate function if there
+    are multiple b vectors to be considered which is very often the case. 
+
+    Args:
+        LU (two dimensional numpy array): factored A matrix; L is the lower
+            triangular with 1's on the diagonal; U is the upper triangular including
+            the values on the diagonal
+        b (one dimensional numpy array): the right hand side; the constant
+            values in the equations
+        ov (one dimensional numpy array): the permutated order of the rows after
+            partial pivoting and factorization
+
+    Returns:
+        b (one dimensional numpy array): often referred to 'c' in literature on
+            the subject; the b vector with the factored operations from the
+            decomposition applied
+
+    """
+    n,m = LU.shape
+    nmo = n - 1
+    for j in xrange(nmo):
+        for i in xrange(j+1,n):
+            b[ov[i]] = b[ov[i]] - LU[ov[i],j]*b[ov[j]]
+
+    return b
+
+
+def back_sub(LU, b, ov):
+    """
+    Back substituation applied to solve for the x vector.
+
+    Solves the upper triangular matrix U with the factored b vector for the x
+    vector. 
+
+    Args:
+        LU (two dimensional numpy array): factored A matrix; L is the lower
+            triangular with 1's on the diagonal; U is the upper triangular including
+            the values on the diagonal
+        b (one dimensional numpy array): often referred to 'c' in literature on
+            the subject; the b vector with the factored operations from the
+            decomposition applied
+        ov (one dimensional numpy array): the permutated order of the rows after
+            partial pivoting and factorization
+
+    Retuns:
+        x (one dimensional numpy array): the solution vector
+    
+    """
+    n,m = LU.shape
     nmo = n - 1
     x = np.empty((n,1),dtype='float')   
     #back substitution
-    x[ov[nmo]] = b[ov[nmo]]/A[ov[nmo],nmo] # last row solution
+    x[ov[nmo]] = b[ov[nmo]]/LU[ov[nmo],nmo] # last row solution
     for j in xrange(nmo-1,-1,-1):
         x[ov[j]] = b[ov[j]] #initialize solution value
         for k in xrange(nmo,j,-1):
             #group known terms in numerator
-            x[ov[j]] = x[ov[j]] - x[ov[k]]*A[ov[j],k] 
-        x[ov[j]] = x[ov[j]]/A[ov[j],j] #solving equation by division
+            x[ov[j]] = x[ov[j]] - x[ov[k]]*LU[ov[j],k] 
+        x[ov[j]] = x[ov[j]]/LU[ov[j],j] #solving equation by division
     
     return x
 
-#def rhs(A, b, ov):
-#    #one may want to incorporate this into the typical flow of this program
-#    #i.e. have one fuction to calc the LU decomposition, one function to apply
-#    #the LU decomposition to the rhs, and one function to complete the back
-#    #substituation to solve the equations; not sure if this will be slow than
-#    #having a seperate function for additional rhs as currently shown
-#    n,m = A.shape
-#    nmo = n - 1
-#    for j in xrange(nmo):
-#        for i in xrange(j+1,n):
-#            b[ov[i]] = b[ov[i]] - A[ov[i],j]*b[ov[j]]
-#
-#    return b
-#def forward_sub():
-#
-#def lu_solve(A, b):
-#    A, ov = lu_decomp(A)
-#    b = rhs(A, b, ov)
-#    x = back_sub(A, b, ov)
+
+def lu_solve(LU, b):
+    """Applies the forward elimination and back substituation steps."""
+    b = for_elim(LU, b, ov)
+    x = back_sub(LU, b, ov)
+    return x
+
 
 if __name__ == "__main__":
-    A = np.array([[0.0,2.0,0.0,1.0],
+    A1 = np.array([[0.0,2.0,0.0,1.0],
                   [2.0,2.0,3.0,2.0],
                   [4.0,-3.0,0.0,1.0],
                   [6.0,1.0,-6.0,-5.0]])
-    print A
-    b = np.array([[0.0],
+    print A1
+    b1 = np.array([[0.0],
                   [-2.0],
                   [-7.0],
                   [6.0]])
-    print b
-    A,b,ov = lu_decomp(A, b)
-    x = back_sub(A, b, ov)
-    print A
+    print b1
+    LU, ov, b, x = lu_solve(A1, b1)
+    print LU
     print ov
     print b
     print x
